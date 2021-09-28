ebf09ca95ff79946cbd20bf000b778b99b1d3794
Cole Enyart <letchro@gmail.com>
2021-09-27 20:16:10 -0500

    added classes to html, added selector and click for save button in javascript

---
+++ script.js
@@ -0,0 +1,22 @@
+// selectors
+var saveButtonEl = $('.saveBtn');
+
+// global variables
+
+// current day
+function currentDay(){
+
+}
+
+// past, present, future
+function aChristmasCarol(){
+
+}
+
+// saveBtn click equals text saved to local storage
+// saved events persist
+saveButtonEl.on('click', function () {
+    // save to local storage
+
+    // output text on refresh?
+});
