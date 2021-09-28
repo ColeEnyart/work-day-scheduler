e45c44f41d30e46cc5fbac79604c8b04d591617d
Cole Enyart <letchro@gmail.com>
2021-09-28 18:57:54 -0500

    Added all hours of work day in html, added function to find current time in js

--- script.js
+++ script.js
@@ -1,12 +1,16 @@
 // selectors
-var saveButtonEl = $('.saveBtn');
-var textAreaEl = $('.text');
+var saveButtonEl = $(".saveBtn");
+var textAreaEl = $(".text");
+var currentDay = $("#currentDay");
 
 // global variables
+var allNotes = [];
 
-// current day
-function currentDay(){
-
+// current time
+function currentTime(){
+    var now = moment();
+    currentDay.text(now.format("MMMM DD, YYYY"));
+    
 }
 
 // past, present, future
@@ -16,9 +20,24 @@ function aChristmasCarol(){
 
 // saveBtn click equals text saved to local storage
 // saved events persist
-saveButtonEl.on('click', function () {
+saveButtonEl.on("click", function () {
     // save to local storage
-    var text = textAreaEl.val();
-    localStorage.setItem('text', text);
+    /* var text = textAreaEl.val();
+    localStorage.setItem('text', text); */
     // output text on refresh?
+
+    $(".text").each( function(){
+        $(this).val();
+
+        // add each value to the allNotes array
+        allNotes.push(this);
+        console.log(allNotes);
+
+        // allNotes into storage stringify
+        var storage = JSON.stringify(allNotes);
+        localStorage.setItem("text", storage);
+
+    })
 });
+
+currentTime();
